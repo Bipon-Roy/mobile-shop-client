5c4b6ed0f41430a2b9f0353d2ee847eea1c3b0d0
Bipon Roy <biponroy400@gmail.com>
2024-01-19 01:31:52 +0600

    Updated Layout

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,16 @@ function App() {
         });
     }, []);
     return (
-        <div className="overflow-hidden">
-            <Navbar />
-            <Outlet />
-            <Footer />
+        <div className="overflow-hidden min-h-screen flex flex-col">
+            <div className="flex-none">
+                <Navbar />
+            </div>
+            <div className="flex-grow">
+                <Outlet />
+            </div>
+            <div>
+                <Footer />
+            </div>
         </div>
     );
 }
