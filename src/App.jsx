e1ff8e8be65d1022718b42870e214b4fd0d8e50c
Bipon Roy <biponroy400@gmail.com>
2024-04-24 21:46:53 +0600

    Fixed navigation issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./Pages/Shared/Navbar/Navbar";
 import Footer from "./Pages/Shared/Footer/Footer";
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 function App() {
+    const location = useLocation();
     // configuration for AOS animation library
     useEffect(() => {
+        window.scrollTo(0, 0); // Scroll to top on route change
         AOS.init({
             delay: 0,
             duration: 1000,
             anchorPlacement: "top-bottom",
             once: true,
         });
-    }, []);
+    }, [location.pathname]);
     return (
         <div className="overflow-hidden min-h-screen flex flex-col">
             <div className="flex-none">
